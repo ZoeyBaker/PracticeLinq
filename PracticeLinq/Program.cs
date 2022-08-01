8c8ce03ea0da5dcd625a5567dd71a92c933d570f
Zoey Baker <zoeybaker625@gmail.com>
2022-08-01 13:56:47 -0500

    initial commit

---
+++ PracticeLinq/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PracticeLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<string> GameNames = new List<string> { "GTA", "RDR2", "Bully", "Spongebob", "Endless Ocean" };
+
+            var LongGameNames =
+                GameNames.OrderBy(x => x.Length);
+
+            foreach (var Length in GameNames) 
+
+            {
+                Console.WriteLine(Length);
+            }
+
+
+        }
+        
+    }
+}
